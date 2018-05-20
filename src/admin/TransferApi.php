cbb7752c71098eb75451306c0fca48325c683e05
wshaman <serge@shtriling.com>
2018-05-20 13:17:40 +0300

    [+] Download file

--- src/admin/TransferApi.php
+++ src/admin/TransferApi.php
@@ -19,6 +19,16 @@ class TransferApi extends BaseApi
 {
     public $defaultMethod = 'log';
     private $_clients = [];
+    private $coins = [];
+    private $locations = [];
+
+    public function __construct($data, $request)
+    {
+        parent::__construct($data, $request);
+        $this->coins = (new Coin())->findAll([], 'id');
+        $this->locations = (new Location())->findAll([], 'id');
+
+    }
 
     private function _getClientById($id)
     {
@@ -29,6 +39,35 @@ class TransferApi extends BaseApi
         return $this->_clients[$id];
     }
 
+
+    private function _processRow($item)
+    {
+        $sender = 'system';
+        $s_coin = null;
+        $receiver = null;
+        if ($item['sender_id']) {
+            $s = $this->_getClientById($item['sender_id']);
+            $loc = $this->locations[$s['location_id']];
+            $s_coin = $this->coins[$s['coin_id']]['code'];
+            $sender = "{$s['fullname']} from {$loc['city']}, {$loc['country']}";
+        }
+        $r = $this->_getClientById($item['receiver_id']);
+
+        $loc = $this->locations[$r['location_id']];
+        $r_coin = $this->coins[$r['coin_id']];
+        $receiver = "{$r['fullname']} from {$loc['city']}, {$loc['country']}";
+
+        $row = [
+            'hash' => $item['hash'],
+            'sender' => $sender,
+            'sender_coin' => $s_coin,
+            'receiver' => $receiver,
+            'receiver_coin' => $r_coin['code'],
+            'amount' => $item['receive_amount'] / $r_coin['precision'],
+        ];
+        return $row;
+    }
+
     public function LogMethodAny()
     {
         $uid = $this->fromAnyRequired('uid');
@@ -36,11 +75,11 @@ class TransferApi extends BaseApi
         $date_to = $this->fromAny('date_to');
         $page = $this->fromAny('page', 1);
         $page = intval($page) - 1;
-        $response = ['total' => 0, 'value_usd' => 0, 'value_coin' => 0, 'rows' => []];
         $limit = 100;
+        $response = ['total' => 0, 'value_usd' => 0, 'value_coin' => 0, 'rows' => [], 'per_page' => $limit,
+            'page' => $page + 1
+        ];
         $client = (new Client())->findOne(['id' => $uid]);
-        $coins = (new Coin())->findAll([], 'id');
-        $locations = (new Location())->findAll([], 'id');
         $usd_coin = (new Coin())->findOne(['code' => 'USD']);
         //@todo: For partman check if limits are in 1 month and use specific table.
         //@todo: Validate all this stuff
@@ -49,7 +88,6 @@ class TransferApi extends BaseApi
         $params = [':uid' => $uid];
         if ($date_from) {
             $wheres .= " AND created_at >= :date_from";
-//            $params[':date_from'] = strtotime($date_from);
             $params[':date_from'] = $date_from;
         }
         if ($date_to) {
@@ -70,36 +108,60 @@ class TransferApi extends BaseApi
         }
         $sums = $tr->query("SELECT SUM(receive_amount * receive_rate_usd / receive_rate_coin) as sum_usd, 
         SUM(receive_amount) as sum_coin FROM {$tr->table} {$wheres}", $params);
-        $response['value_usd'] = $sums[0]['sum_usd'] * $coins[$client['coin_id']]['precision'] / $usd_coin['precision'];
-        $response['value_coin'] = $sums[0]['sum_coin'] / $coins[$client['coin_id']]['precision'];
+        $response['value_usd'] = $sums[0]['sum_usd'] * $this->coins[$client['coin_id']]['precision'] / $usd_coin['precision'];
+        $response['value_coin'] = $sums[0]['sum_coin'] / $this->coins[$client['coin_id']]['precision'];
 
 
         $items = $tr->query("SELECT a.* FROM {$tr->table} AS a {$wheres} {$paginate}", $params);
         foreach ($items as $k => $item) {
-            $sender = 'system';
-            $s_coin = null;
-            $receiver = null;
-            if ($item['sender_id']) {
-                $s = $this->_getClientById($item['sender_id']);
-                $loc = $locations[$s['location_id']];
-                $s_coin = $coins[$s['coin_id']]['code'];
-                $sender = "{$s['fullname']} from {$loc['city']}, {$loc['country']}";
-            }
-            $r = $this->_getClientById($item['receiver_id']);
-
-            $loc = $locations[$r['location_id']];
-            $r_coin = $coins[$r['coin_id']];
-            $receiver = "{$r['fullname']} from {$loc['city']}, {$loc['country']}";
-
-            $response['rows'][] = [
-                'hash' => $item['hash'],
-                'sender' => $sender,
-                'sender_coin' => $s_coin,
-                'receiver' => $receiver,
-                'receiver_coin' => $r_coin['code'],
-                'amount' => $item['receive_amount'] / $r_coin['precision']
-            ];
+            $response['rows'][] = $this->_processRow($item);
         }
         return $response;
+    }
+
+    public function DownloadMethodAny()
+    {
+        $uid = $this->fromAnyRequired('uid');
+        $date_from = $this->fromAny('date_from');
+        $date_to = $this->fromAny('date_to');
+        $client = (new Client())->findOne(['id' => $uid]);
+        $usd_coin = (new Coin())->findOne(['code' => 'USD']);
+        //@todo: For partman check if limits are in 1 month and use specific table.
+        //@todo: Validate all this stuff
+        $wheres = " WHERE (sender_id = :uid OR receiver_id =:uid)";
+        $paginate = '';
+        $params = [':uid' => $uid];
+        if ($date_from) {
+            $wheres .= " AND created_at >= :date_from";
+            $params[':date_from'] = $date_from;
+        }
+        if ($date_to) {
+            $wheres .= " AND created_at < :date_to";
+            $params[':date_to'] = $date_to;
+        }
+        $tr = new Transaction();
+        $count = $tr->query("SELECT COUNT(*) as cnt FROM {$tr->table} {$wheres}", $params);
+        $count = $count[0]['cnt'];
+        //@todo: use cursor
+        $limit = 1000;
+        $steps = ceil($count / $limit);
+        $query = "SELECT a.* FROM {$tr->table} AS a {$wheres} ";
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment; filename=file.csv");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        for ($i = 0; $i < $steps; $i++) {
+            $paginate = " LIMIT :limit OFFSET :offset";
+            $params[':limit'] = $limit;
+            $params[':offset'] = $limit * $i;
+            $rows = $tr->query($query . $paginate, $params);
+
+            foreach ($rows as $row) {
+                $item = $this->_processRow($row);
+                $data = "{$item['hash']};{$item['sender']};{$item['receiver']};{$item['amount']}\n";
+                echo $data;
+            }
+        }
+        die;
     }
 }
--- web/static/main.js
+++ web/static/main.js
@@ -1,3 +1,10 @@
+const C = {
+    urls: {
+        clientList: "/admin/client/list",
+        downloadCurrent: "/admin/transfer/download"
+    }
+};
+
 $(document).ready(function () {
     $(function () {
         $(".datepicker").datepicker({dateFormat: 'yy-mm-dd'});
@@ -6,7 +13,7 @@ $(document).ready(function () {
     loadClients();
     $("form").submit(function (e) {
         let self = this;
-        let url = $(this).attr('action') +'?'+ $(self).serialize();
+        let url = $(this).attr('action') + '?' + $(self).serialize();
         console.log(url);
         getjson(url, function (data) {
             putLog(data);
@@ -14,19 +21,18 @@ $(document).ready(function () {
         e.preventDefault();
         return false;
     });
+    $('a#download-current').click(function (e) {
+        let frm = $("form#form_log");
+        $(this).attr('href', C.urls.downloadCurrent + '?' + frm.serialize())
+        return true;
+    })
 });
 
-const C={
-    urls: {
-        clientList:"/admin/client/list"
-    }
-};
-
-function showError(msg){
+function showError(msg) {
     alert(msg);
 }
 
-function getjson(url, cb){
+function getjson(url, cb) {
     $.getJSON(url, function (data) {
         if (typeof data['error'] != 'undefined') {
             showError(data['error'])
@@ -36,12 +42,12 @@ function getjson(url, cb){
     })
 }
 
-function loadClients(){
-    getjson(C.urls.clientList, function(data){
+function loadClients() {
+    getjson(C.urls.clientList, function (data) {
         console.log(data);
         let sel_obj = $('select#user_list');
         sel_obj.html('');
-        for(let i=0; i<data.length; i++){
+        for (let i = 0; i < data.length; i++) {
             sel_obj.append(`<option value="${data[i].id}">${data[i].name}</option>`)
         }
     });
@@ -51,11 +57,17 @@ function putLog(data) {
     console.log(data);
     let tbl_obj = $('table#table-data tbody');
     let tbl_stat = $('table#table-stat tbody tr');
+    let sel_pager = $('select#pager-selector');
+    let page_count = Math.ceil(data.total / data.per_page);
     tbl_obj.html('');
     $('.coin-name').html(data.receiver_coin);
     tbl_stat.find('td').eq(0).html(data.value_coin);
     tbl_stat.find('td').eq(1).html(data.value_usd);
-    for(let i=0; i< data.rows.length; i++){
+    sel_pager.html('');
+    for (let i = 1; i <= page_count; i++) {
+        sel_pager.append(`<option value="${i}">${i}</option>`);
+    }
+    for (let i = 0; i < data.rows.length; i++) {
         let itm = data.rows[i];
         tbl_obj.append(`<tr><td>${itm.hash}</td><td>${itm.sender}</td><td>${itm.receiver}</td><td>${itm.amount}</td><td></td></tr>`);
     }
